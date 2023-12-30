ce216ad358c488342f33255cd780777a4ca8f5e3
Matías Jordán <matiasowjordan@gmail.com>
2023-12-30 10:37:04 -0300

    imagenes webp

--- src/Components/Motivo.jsx
+++ src/Components/Motivo.jsx
@@ -1,6 +1,16 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
+
 function Motivo({motivo}) {
+     useEffect(() => {
+          AOS.init({
+               duration: 1000,
+          })
+     })
+
      return (
-          <article className="motivo w-full flex h-64 even:flex-row-reverse even:text-right  odd:text-left items-center justify-between">
+          <article className="motivo w-full flex h-64 even:flex-row-reverse even:text-right  odd:text-left items-center justify-between" data-aos="fade-down">
                <div className="textoMotivo w-2/4">
                     <p className="text-[#303030] text-3xl">{motivo.texto}</p>
                </div>
