87bd346084836356eee5c6b69acc5ae94f133b4c
Matías Jordán <matiasowjordan@gmail.com>
2024-02-16 13:11:06 -0300

    correcion cors

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = 3000;
 
 app.use(cors({
-	origin: 'http://localhost:5174',
+	origin: 'https://main--damiannahueljordan.netlify.app/presupuestoClases',
 	credentials: true,
 }));
 app.use(express.json());
